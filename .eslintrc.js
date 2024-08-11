044b1573531ece5ff355af457b1847b38aa2d379
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-08-11 13:51:15 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'strings must use singlequote': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off',
+    'no-unused-vars': 'off',
+    'no-irregular-whitespace': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    // eslint-disable-next-line quote-props
+    'indent': 'off'
+  }
+}
