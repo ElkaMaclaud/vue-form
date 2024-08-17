7f3c92e995b2f6d5c376a5fb6a50ac634c6c375a
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-08-17 22:10:26 +0300

    plugins

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,21 @@
 <template>
   <div class="container">
-    <div class="card" v-if="show">
-      <h2 v-color:[type].blink.hover="myColor">Разговор про директивы</h2>
+    <div class="card">
+      <h2>{{ $i18n('app.title') }}</h2>
 
-      <div class="form-control">
-        <label for="inp">Активный по умолчанию</label>
-        <input v-focus type="text" id="inp">
-      </div>
-
-      <button class="btn" @click="myColor='darkblue'">Сделать синим!</button>
-      <button class="btn" @click="type = type ==='color' ? 'backgroundColor' : 'color'">Переключить тип!</button>
+      <button class="btn" @click="changeLang">{{$i18n('app.changeBtn')}}</button>
     </div>
   </div>
 </template>
 
 <script>
-import focusDirective from './focusDirective'
-import colorDirective from './colorDirective'
-
 export default {
-  data() {
-    return {
-      myColor: 'darkred',
-      type: 'color',
-      show: true
+  inject: ['changeI18N'],
+  methods: {
+    changeLang() {
+      this.changeI18N('en')
+      this.$forceUpdate()
     }
-  },
-  // mounted() {
-  //   setTimeout(() => {
-  //     this.show = false
-  //   }, 2000)
-  // },
-  directives: {
-    focus: focusDirective,
-    color: colorDirective
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import translatePlugin from './translatePlugin'
 import './theme.css'
 
-createApp(App)
-    // .directive('focus', {
-    //     mounted(el) {
-    //         el.focus()
-    //     }
-    // })
-    .mount('#app')
+const app = createApp(App)
+
+const ru = {
+    app: {
+        title: 'Как работают плагины во Vue?',
+        changeBtn: 'Переключить язык'
+    }
+}
+const en = {
+    app: {
+        title: 'How plugind work in Vue?',
+        changeBtn: 'Toggle Language'
+    }
+}
+
+app.use(translatePlugin, { ru, en })
+
+app.mount('#app')
+
+// app.directive('focus', {
+//     mounted(el) {
+//         el.focus()
+//     }
+// })
