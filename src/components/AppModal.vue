0193ccb554c8be1968cd2a6aa74a553e8d7e0785
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-08-18 13:15:32 +0300

    teleport

---
+++ src/components/AppModal.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="modal-backdrop" @click="$emit('close')"></div>
+    <div class="modal">
+        <h3>Это заголовок модалки</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia distinctio tempore dolores, harum vitae fugiat deleniti praesentium quisquam ab! Maxime!</p>
+    </div>
+</template>
+<script>
+export default {
+    emits: ['close']
+}
+</script>
+
+<style lang="">
+
+</style>
